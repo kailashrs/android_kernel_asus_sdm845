faf99b7733c42a48593bd833b1dbd88d22688a05
Roopesh Rajashekharaiah Nataraja <roopeshr@codeaurora.org>
2018-11-22 10:19:42 +0530

    rpmh_master_stat: Compare previous sleep data before updating
    APPS accumulated sleep duration gets updated even when APPS
    did not execute cx-off sequence.
    
    Add a check to compare previous sleep time stamp with latest
    timestamp before updating sleep information.
    
    Bug: 110159524
    
    Change-Id: I4f720e219eee104c9a53a67aca8d34c3c6cf2542
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>
    Signed-off-by: Roopesh Rajashekharaiah Nataraja <roopeshr@codeaurora.org>
    Signed-off-by: Redick Lin <redicklin@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/soc/qcom/rpmh_master_stat.c
+++ drivers/soc/qcom/rpmh_master_stat.c
@@ -192,6 +192,10 @@ static void asus_dump_rpmh_master_stats (void)
 static inline void msm_rpmh_apss_master_stats_update(
 				struct msm_rpmh_profile_unit *profile_unit)
 {
+	if (apss_master_stats.last_entered ==
+					profile_unit[POWER_DOWN_END].value)
+		return;
+
 	apss_master_stats.counts++;
 	apss_master_stats.last_entered = profile_unit[POWER_DOWN_END].value;
 	apss_master_stats.last_exited = profile_unit[POWER_UP_START].value;
