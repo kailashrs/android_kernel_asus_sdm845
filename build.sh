4fa80309a158bc44a71284ef43a0d4b67c7bd537
Kailash <kailash.sudhakar@gmail.com>
2019-12-22 14:27:57 +0530

    Use ccache

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 export ARCH="arm64"
 export CROSS_COMPILE="/home/kailashrs/5z/aarch64-elf-gcc/bin/aarch64-elf-"
+export CCACHE_DIR=/home/kailashrs/5z/ccache
+export CC="ccache gcc"
+export CXX="ccache g++"
 make O=out Z01R_defconfig -j8
 make O=out -j8
