a5fdb42dc8f7ed4b2925710b42cd39381a98a704
Kailash <kailash.sudhakar@gmail.com>
2019-12-23 19:14:41 +0530

    Update build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export CROSS_COMPILE="/home/kailashrs/5z/aarch64-elf-gcc/bin/aarch64-elf-"
 export CROSS_COMPILE_ARM32="/home/kailashrs/5z/arm-eabi-gcc/bin/arm-eabi-"
 export CC="ccache gcc"
 export CXX="ccache g++"
-make O=out Z01R_defconfig -j8
+make O=out Z01R_defconfig
 make O=out -j8
