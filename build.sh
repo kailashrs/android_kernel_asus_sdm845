6682f3bf377403e3e674c6d49af18b6caabef832
Kailash <kailash.sudhakar@gmail.com>
2019-12-24 11:58:03 +0530

    Update build script
    This reverts commit 7e9b2971b2ef7700e8af7cf6cfb388f03c7a9f33.

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 export ARCH="arm64"
-export CROSS_COMPILE="/home/kailashrs/5z/aarch64-elf-gcc/bin/aarch64-elf-"
-export CROSS_COMPILE_ARM32="/home/kailashrs/5z/arm-eabi-gcc/bin/arm-eabi-"
-export CC="ccache gcc"
-export CXX="ccache g++"
+export PATH="/usr/lib/ccache/bin:$PATH"
+export CROSS_COMPILE="ccache /home/kailashrs/5z/aarch64-elf-gcc/bin/aarch64-elf-"
+export CROSS_COMPILE_ARM32="ccache /home/kailashrs/5z/arm-eabi-gcc/bin/arm-eabi-"
 make O=out Z01R_defconfig
 make O=out -j8
