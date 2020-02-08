d1008796859f2d41e787e65908c8722c718fd151
Panchajanya1999 <panchajanya@azure-dev.live>
2020-02-08 15:14:20 +0530

    cpufreq: schedutil: Expose limit tunables cluster-wise
    This allows us to tune the default rate-limits for each cluster
    without constantly changing schedutil's code.
    
    Idea taken from : kdrag0n/proton_zf6@d9faa70#diff-d96c15a8e1af1ce4aa4750bef6b7e38d
    [panchajanya1999] : Allow tuning for each cluster
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -16,6 +16,7 @@
 #include <linux/slab.h>
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
+#include <linux/binfmts.h>
 #include "sched.h"
 #include "tune.h"
 
@@ -457,8 +458,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	/* Don't let userspace change this */
-	return count;
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -480,8 +482,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	/* Don't let userspace change this */
-	return count;
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -718,10 +721,19 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	tunables->iowait_boost_enable = false;
 
-	tunables->up_rate_limit_us =
-				CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	tunables->down_rate_limit_us =
-				CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
+	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
+		tunables->up_rate_limit_us =
+					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_BIG;
+		tunables->down_rate_limit_us =
+					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_BIG;
+	}
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
+		tunables->up_rate_limit_us =
+					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_LITTLE;
+		tunables->down_rate_limit_us =
+					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_LITTLE;
+	}
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
